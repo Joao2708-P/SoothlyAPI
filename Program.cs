de34af116611fc924414cd9b92b4c8134c547099
JOAO GUILHERME <u21238@academico.cotuca.unicamp.br>
2022-06-28 17:51:11 -0300

    updaate4.0

--- Program.cs
+++ Program.cs
@@ -5,10 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+// Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins, builder =>{
+          builder.WithOrigins("http://localhost").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+          builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+          builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+          builder.SetIsOriginAllowed(origin => true);
+    });
+});
 
 //Conect!
 builder.Services.AddDbContext<SoothlyContext>(options =>
@@ -26,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
